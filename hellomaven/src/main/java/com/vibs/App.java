be69515c0450d4271f3f6f2118f51ea919da8f1f
vibhakarmishra <root@ip-172-31-43-184.us-east-2.compute.internal>
2021-05-26 13:18:12 +0000

    code change3

--- hellomaven/src/main/java/com/vibs/App.java
+++ hellomaven/src/main/java/com/vibs/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! How's your night/day?" );
+	System.out.println( "Good Bye!!" );
     }
 }
